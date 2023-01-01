ff9e11170ca7968d8266344d5d9610e7f7256c22
a-sadri <asadri721@gmail.com>
2023-01-01 12:25:51 +0330

    Add some server boilerplates

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
 // Configuration
 dotenv.config();
 
@@ -17,6 +22,12 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+// Routes
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
 
 const PORT = process.env.PORT;
 
